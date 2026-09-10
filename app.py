bbaecc526443752f6e2eabb961d82164148e0077
DarkCore <146465481+cosmicfoxzilla@users.noreply.github.com>
2026-09-10 17:51:31 +0530

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -373,7 +373,8 @@ def editpage(project_id):
     return render_template("edit.html", projectData = project)
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
